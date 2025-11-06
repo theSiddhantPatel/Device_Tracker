0aa3418cbba83295c611ec2b782bc5eb6223fbe8
Siddhant Patel <spatel44427777@gmail.com>
2025-11-06 19:17:03 +0530

    script updated

--- public/js/script.js
+++ public/js/script.js
@@ -16,7 +16,7 @@ if (navigator.geolocation) {
 const map = L.map("map").setView([0, 0], 10);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "Sheriyans Coding School",
+  attribution: "Siddhant Patel with Sheriyans",
 }).addTo(map);
 
 const markers = {};
